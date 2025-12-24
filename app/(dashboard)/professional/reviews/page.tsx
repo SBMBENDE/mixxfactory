a4edd854f074fde9364354d8d50fb0d6b43ad4a3
mbende sampson <mbende2000@yahoo.com>
2025-12-24 14:50:21 +0100

    Fix: Remove unused faTimes icon import

--- app/(dashboard)/professional/reviews/page.tsx
+++ app/(dashboard)/professional/reviews/page.tsx
@@ -6,7 +6,7 @@
 
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTimes, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faFilter } from '@fortawesome/free-solid-svg-icons';
 
 interface Review {
   _id: string;
